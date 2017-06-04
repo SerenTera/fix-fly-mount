1eb788e43c65d4a3ca9b4302b0dea3689b8466b9
SerenTera <SerenTera@users.noreply.github.com>
2017-06-04 16:06:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 module.exports = function flymountfix(dispatch) {
 
-let flyvar,
-	flyunk,
-	cID;
+let flyvar;
+let	uservar=[];
+	
+	dispatch.hook('S_LOGIN', 1, event => {
+		player = event.playerId;
+	});
 	
 	dispatch.hook('S_USER_FLYING_LOCATION',1, event => {
-		if(event.type===7 || event.type===8) {
-			flyunk = event.unk1,
-			cID = event.target;
-			return false;
-		};
-	});	
-	
-	dispatch.hook('C_PLAYER_FLYING_LOCATION',3, event => {
-		if(event.type===7 || event.type===8) {
-			flyvar=event,
-			delete flyvar.time,
-			dispatch.toClient ('S_USER_FLYING_LOCATION',1, {
-				target: cID,
-				flyvar,
-				unk1: flyunk,
-				unk2: 0x42700000
+		if(event.type===7) {
+			dispatch.hook('C_USER_LOCATION',1, event => {
+				flyvar = event.w;
+			});		
+		}
+		if(event.type===8) {
+			dispatch.hook('C_PLAYER_LOCATION',1, event => {
+				uservar = event,
+				wedit();
+				return;
 			});
 		};
 	});
+	
+	function wedit() {
+		dispatch.toClient('S_USER_LOCATION',1, {
+			target: player,
+			x1: uservar.x1,
+			y1: uservar.y1,
+			z1: uservar.z1,
+			w: flyvar,
+			unk2: uservar.unk2,
+			speed: uservar.speed,
+			x2: uservar.x2,
+			y2: uservar.y2,
+			z2: uservar.z2,
+			type: uservar.type,
+			unk: 0,
+		});
+	};
 };
